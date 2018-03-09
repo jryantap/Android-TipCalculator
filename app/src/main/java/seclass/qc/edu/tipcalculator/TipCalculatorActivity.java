ad3f961bf39dc6a040dfc50365f0bf4627e26c6a
jryantap <jhericot@outlook.com>
2018-03-09 09:48:45 -0500

    commit

--- app/src/main/java/seclass/qc/edu/tipcalculator/TipCalculatorActivity.java
+++ app/src/main/java/seclass/qc/edu/tipcalculator/TipCalculatorActivity.java
@@ -2,13 +2,14 @@ package seclass.qc.edu.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class TipCalculatorActivity extends AppCompatActivity {
 
     EditText checkAmountValue, fifteenPercentTipValue, twentyPercentTipValue, twentyFivePercentTipValue;
-    EditText partySizevalue, fifteenPercentTotalTipValue, twentyPercentTotalTipValue, twentyFivePercentTipTotalValue;
+    EditText partySizeValue, fifteenPercentTotalTipValue, twentyPercentTotalTipValue, twentyFivePercentTipTotalValue;
     Button buttonCompute;
 
     int checkAmount, partySize, fifteenPercentTip, twentyPercentTip, twentyFivePercenTip;
@@ -20,6 +21,27 @@ public class TipCalculatorActivity extends AppCompatActivity {
 
         //get all views
         checkAmountValue = findViewById(R.id.checkAmountValue);
+        partySizeValue = findViewById(R.id.partySizeValue);
+        buttonCompute = findViewById(R.id.buttonCompute);
+        //tip values
+        fifteenPercentTipValue = findViewById(R.id.fifteenPercentTipValue);
+        twentyPercentTipValue = findViewById(R.id.twentyPercentTipValue);
+        twentyFivePercentTipValue = findViewById(R.id.twentyFivePercentTipValue);
+        //total values
+        fifteenPercentTotalTipValue = findViewById(R.id.fifteenPercentTotalValue);
+        twentyPercentTotalTipValue = findViewById(R.id.twentyPercentTotalValue);
+
+        buttonCompute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String amount = checkAmountValue.getText().toString();
+                String party = partySizeValue.getText().toString();
+
+                
+            }
+        });
+
+
 
     }
 }
